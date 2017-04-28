dbf91fdce97dbffb7f25b4eea2e192c85c661480
Rafał Staroń <s12570@pjwstk.edu.pl>
2017-04-28 08:43:27 +0200

    First

---
+++ MAS3/MAS3/Osoba.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAS3
+{
+    abstract class Osoba
+    {
+        public readonly string pesel;
+        public string imie { get; private set; }
+        public string nazwisko { get; private set; }
+        public Osoba(string imie, string nazwisko, string pesel)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.pesel = pesel;
+        }
+    }
+}
---
+++ MAS3/MAS3/Pracownik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAS3
+{
+    class Pracownik : Osoba
+    {
+        public double stawkaGodzinowa { get; private set; }
+        public Pracownik(string imie, string nazwisko, string pesel,double stawka ) : base(imie, nazwisko, pesel)
+        {
+            stawkaGodzinowa = stawka;
+
+        }
+    }
+}
